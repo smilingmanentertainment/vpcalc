77b47cd16d29ae1e0610b4c93db57d0548c8aa52
Jacob Denholtz <jdenholtz@email.arizona.edu>
2018-02-17 23:55:25 +0000

    game page added, links hooked up

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
 root 'static_pages#index'
 
+get 'games', to: 'static_pages#games'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
